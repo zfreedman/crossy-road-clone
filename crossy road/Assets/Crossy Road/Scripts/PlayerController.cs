f4052367085d8dbfaf0168a21fc7b9d19abc1314
zach freedman <zfreedman92@gmail.com>
2018-11-04 21:33:44 -0800

    feat: Began stubbing out public/private members for CharacterController

--- crossy road/Assets/Crossy Road/Scripts/PlayerController.cs
+++ crossy road/Assets/Crossy Road/Scripts/PlayerController.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public  GameObject      chick = null;
+    public  float           colliderDistCheck = 1;
+    public  bool            isDead = false;
+    public  bool            isIdle = true;
+    public  bool            isJumping = false;
+    public  bool            isMoving = false;
+    public  bool            jumpStart = false;
+    public  ParticleSystem  particle = null;
+    public  float           moveTime = 0.4f;
+    public  float           moveDistance = 1;
+
+    private bool            isVisible = false;
+    private Renderer        localRenderer = null; 
+
 	// Use this for initialization
 	void Start () {
 		
