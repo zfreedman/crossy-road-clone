658b1985e3b5ca5190dc100930be62fd8d88fced
zach freedman <zfreedman92@gmail.com>
2018-11-04 22:39:08 -0800

    feat: Implemented first pass on character movement

--- crossy road/Assets/Crossy Road/Scripts/PlayerController.cs
+++ crossy road/Assets/Crossy Road/Scripts/PlayerController.cs
@@ -20,11 +20,61 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CanMove();
 	}
+
+    void CanIdle ()
+    {
+
+    }
+
+    void CanMove ()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) move += transform.forward;
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) move -= transform.forward;
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) move += transform.right;
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) move -= transform.right;
+        if (move != Vector3.zero) Move(move);
+    }
+
+    void CheckIfCanMove ()
+    {
+
+    }
+
+    public void GotHit ()
+    {
+
+    }
+
+    void IsVisible ()
+    {
+
+    }
+
+    void Move (Vector3 move)
+    {
+        LeanTween.move(this.gameObject, transform.position + move, moveTime);
+    }
+
+    void MoveComplete ()
+    {
+
+    }
+
+    void SetMove ()
+    {
+
+    }
+
+    void SetMoveForwardState ()
+    {
+
+    }
 }
